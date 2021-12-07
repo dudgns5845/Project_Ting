e868a3244ea5ef4b032f23a5ea4feb8d87ad5085
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2021-12-07 17:24:19 +0900

    Project_Ting_Sub init
    목표  캐릭터 커스텀 및 파이어베이스 셋팅하기

---
+++ CharacterCustom/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        FSM();
+    }
+
+    void FSM()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+            anim.SetBool("walk", true);
+        if (Input.GetKeyUp(KeyCode.W))
+            anim.SetBool("walk", false);
+
+        if (Input.GetKey(KeyCode.Space))
+            anim.SetBool("jump", true);
+        if (Input.GetKeyUp(KeyCode.Space))
+            anim.SetBool("jump", false);
+
+        if (Input.GetKey(KeyCode.Alpha1))
+            anim.SetBool("shuffling", true);
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+            anim.SetBool("shuffling", false);
+
+        if (Input.GetKey(KeyCode.Alpha2))
+            anim.SetBool("excited", true);
+        if (Input.GetKeyUp(KeyCode.Alpha2))
+            anim.SetBool("excited", false);
+
+        if (Input.GetKey(KeyCode.Alpha3))
+            anim.SetBool("angry", true);
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+            anim.SetBool("angry", false);
+    }
+}
