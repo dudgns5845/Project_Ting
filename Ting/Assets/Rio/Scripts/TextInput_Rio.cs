00e08ba63fc46ee44422a6bb7e981aa3d2b3c52c
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2022-01-10 18:07:10 +0900

    commit

--- Ting/Assets/Rio/Scripts/TextInput_Rio.cs
+++ Ting/Assets/Rio/Scripts/TextInput_Rio.cs
@@ -11,27 +11,10 @@ public class TextInput_Rio : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        keyborad = GameObject.Find("keyboard");
+
         keyborad.SetActive(true);
         keyborad.GetComponent<Keyboard_Rio>().inputField = this.inputField;
         keyborad.GetComponent<Keyboard_Rio>().total_text = inputField.text;
     }
-
-
-
-
-
-    //private void Start()
-    //{
-    //    EventTrigger eventTrigger = gameObject.AddComponent<EventTrigger>();
-
-    //    EventTrigger.Entry entry_PointerDown = new EventTrigger.Entry();
-    //    entry_PointerDown.eventID = EventTriggerType.PointerDown;
-    //    entry_PointerDown.callback.AddListener((data) => { OnPointerDown((PointerEventData)data); });
-    //    eventTrigger.triggers.Add(entry_PointerDown);
-    //}
-
-    //void OnPointerDown(PointerEventData data)
-    //{
-    //    keyborad.SetActive(true);
-    //}
 }
