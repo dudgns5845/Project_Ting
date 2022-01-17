2b0bfe61e077ff700f0ab6f9be83702959b3c21b
Honeyoung_0117 <48305428+dudgns5845@users.noreply.github.com>
2022-01-17 18:16:53 +0900

    player animation 적용

--- Ting/Assets/Rio/Scripts/PlayerMove_Rio.cs
+++ Ting/Assets/Rio/Scripts/PlayerMove_Rio.cs
@@ -18,68 +18,47 @@ public class PlayerMove_Rio : MonoBehaviour
     }
     void Update()
     {
-        //if (anim == null) return;
-        //FSM();
-        //Move();
+        if (anim == null) return;
+        FSM();
         playerMove();
         playerRot();
     }
 
     void FSM()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch) != Vector2.zero)
             anim.SetBool("walk", true);
-        if (Input.GetKeyUp(KeyCode.W))
+        else if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.LTouch) == Vector2.zero)
             anim.SetBool("walk", false);
-
-        if (Input.GetKey(KeyCode.Space))
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger, OVRInput.Controller.All))
             anim.SetBool("jump", true);
-        if (Input.GetKeyUp(KeyCode.Space))
+        else if (OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger, OVRInput.Controller.All))
             anim.SetBool("jump", false);
 
-        if (Input.GetKey(KeyCode.Alpha1))
-            anim.SetBool("shuffling", true);
-        if (Input.GetKeyUp(KeyCode.Alpha1))
-            anim.SetBool("shuffling", false);
+        //if (Input.GetKeyDown(KeyCode.W))
+        //    anim.SetBool("walk", true);
+        //if (Input.GetKeyUp(KeyCode.W))
+        //    anim.SetBool("walk", false);
+        //if (Input.GetKey(KeyCode.Space))
+        //    anim.SetBool("jump", true);
+        //if (Input.GetKeyUp(KeyCode.Space))
+        //    anim.SetBool("jump", false);
 
-        if (Input.GetKey(KeyCode.Alpha2))
-            anim.SetBool("excited", true);
-        if (Input.GetKeyUp(KeyCode.Alpha2))
-            anim.SetBool("excited", false);
+        //if (Input.GetKey(KeyCode.Alpha1))
+        //    anim.SetBool("shuffling", true);
+        //if (Input.GetKeyUp(KeyCode.Alpha1))
+        //    anim.SetBool("shuffling", false);
 
-        if (Input.GetKey(KeyCode.Alpha3))
-            anim.SetBool("angry", true);
-        if (Input.GetKeyUp(KeyCode.Alpha3))
-            anim.SetBool("angry", false);
+        //if (Input.GetKey(KeyCode.Alpha2))
+        //    anim.SetBool("excited", true);
+        //if (Input.GetKeyUp(KeyCode.Alpha2))
+        //    anim.SetBool("excited", false);
+
+        //if (Input.GetKey(KeyCode.Alpha3))
+        //    anim.SetBool("angry", true);
+        //if (Input.GetKeyUp(KeyCode.Alpha3))
+        //    anim.SetBool("angry", false);
     }
-
-    //void Move()
-    //{
-    //    controller = GetComponent<CharacterController>();
-    //    if (controller == null) return;
-    //    if (controller.isGrounded)
-    //    {
-    //        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-    //        moveDirection = transform.TransformDirection(moveDirection);
-    //        moveDirection *= speed;
-    //        if (Input.GetButton("Jump"))
-    //            moveDirection.y = jumpSpeed;
-
-    //    }
-    //    moveDirection.y -= gravity * Time.deltaTime;
-    //    controller.Move(moveDirection * Time.deltaTime);
-
-    //    if (Input.GetKeyDown(KeyCode.E))
-    //    {
-    //        transform.Rotate(new Vector3(0, 30, 0));
-    //    }
-    //    if (Input.GetKeyDown(KeyCode.Q))
-    //    {
-    //        transform.Rotate(new Vector3(0, -30, 0));
-    //    }
-    //}
-
-
 
     void playerMove()
     {
@@ -92,7 +71,7 @@ public class PlayerMove_Rio : MonoBehaviour
         //if (Input.GetButton("Jump"))
         //    moveDirection.y = jumpSpeed;
 
-        if(OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger, OVRInput.Controller.All))
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger, OVRInput.Controller.All))
             moveDirection.y = jumpSpeed;
 
         moveDirection.y -= gravity * Time.deltaTime;
@@ -117,11 +96,11 @@ public class PlayerMove_Rio : MonoBehaviour
     Vector3 rot;
     void rotPlus()
     {
-        rot.y += 45;
+        rot.y += 30;
     }
     void rotMinus()
     {
-        rot.y -= 45;
+        rot.y -= 30;
     }
 }
 
